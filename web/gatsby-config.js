311b262e6048157c28f817c480a60eba04fbdbd1
Alex Crocker <hello@crock.dev>
2020-05-14 14:37:12 -0400

    :bug: gtag configuration

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -13,32 +13,10 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-154623356-2',
-        head: true,
-        anonymize: true,
-        respectDNT: true,
-        exclude: [],
-        pageTransitionDelay: 0,
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        defer: false,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: '.domaincord.org'
-      }
-    },
-    {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         id: 'GTM-NM6DZGG',
-        includeInDevelopment: true,
-        defaultDataLayer: {platform: 'gatsby'},
-        gtmAuth: 'hrEMoq93kQpqeqRsJ5rK8Q',
-        gtmPreview: 'env-3',
-        dataLayerName: 'dataLayer'
+        includeInDevelopment: false
       }
     },
     {
