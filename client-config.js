589c7f59dac09cb153b9ecf9c79e97cba4ea41f0
Alex Crocker <hello@crock.dev>
2020-05-22 15:12:08 -0400

    Moved Sanity Studio to separate repo

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || '8usn3m9a',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+    dataset: process.env.GATSBY_SANITY_DATASET || 'community'
   }
 }
